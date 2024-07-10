92a193e583af209b3f0e70c311abb904446b30e3
david <machadodavid32@gmail.com>
2024-07-10 11:09:09 -0300

    atualizando recebendo dados de usuario

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,17 @@
+# Abaixo, recebendo uma string e somando variaveis com numeros reais
+print("Por favor, informe seu nome")
+nome = input()
+print(f'Seu nome é  {nome}')
+
+print("Agora informe sua idade: ")
+idade = float(input())
+print(f'E sua idade é {idade}')
+
+print("Qual a idade de seu filho?")
+filho = float(input())
+print(f"A idade de seu filho é: {filho}")
+
+print("Agora vamos somar as idades")
+soma = idade + filho
+
+print(f'A soma das idades é: {soma}')
