24f9f141ca0a704ec0b01abade6ca33a0b13bcc2
david <machadodavid32@gmail.com>
2024-07-15 09:47:00 -0300

    Relembrando listas parte  2

--- listas.py
+++ listas.py
@@ -1,3 +1,4 @@
+"""
 david = "bom"
 lista1 = [1, 1, 3 ,4 ,5, 10, 100, 3, 56.7]
 
@@ -15,6 +16,64 @@ print(lista1.count(3)) # comando count() lista quantos números 3 tem na lista
 
 lista1.append(123)
 print(lista1)
+"""
+
+lista1 = [1, 1, 3 ,4 ,5, 10, 100, 3, 56.7]
+lista2 = ([5, 6, 8])
+if [5, 6, 8] in lista2:
+    print("Achei")
 
 
+lista2.extend([12, 44, 57, "Aline", "Arthur"])
+print(lista2)
+    
+# Com append vc pode adicionar apenas um elemento. Com extend vc pode adicionar mais de um elemento
+# usando [] exemplo. lista2 = [12, 44, 55] vai adicionar estes 3 em vez de um.
+
+lista2.insert(3, "machado") # insert tem dois arugmentos, vc escolhe em qual indice via adicionar novo elemento
+print(lista2)    
+
+lista3 = lista1 + lista2 # vai criar uma nova lista com a soma das lista
+print("lista 3 é #######", lista3)
+
+lista3.reverse()
+print("Lista 3 reversa é ###### ",lista3)
+
+print(lista1[::-1]) #outro modo de inverter as lista
+print(lista2[::-1])
+
+lista6 = lista2.copy() # copia todos os dados de uma lista em uma nova lista
+print(lista6)
+
+print(len(lista6)) # Conta quantos elementos tem na lista
+
+lista6.pop() # sem colocar argumento, ele elimina o ultimo.
+print("Eliminando ultimo item lista 6", lista6)
+
+lista1.pop(1) # elimina elemento no indice1
+print(lista1)
+
+lista7 = ["Romario", "Bebeto", "Zinho", "Mazinho"]
+lista7.clear() # Vai zerar a lista
+print(lista7)
+
+lista = []
+tuplas = ()
+
+lista8 = [1, 3, 5, 15]
+lista8 = lista8 * 3  # multiplica a lista
+print(lista8)
+
+
+# Converter uma STRING em uma lista[]
+lista9 = "David de Araújo Machado"
+print(lista9.split()) # split automaticamente transforma um texto em lista separando por espaços.
+# resposta: ['David', 'de', 'Araújo', 'Machado']
+
+# Transformando uma lista em uma STRING
+lista10 = "".join(lista9) # aqui coloquei espaço, mas pode colocar qualquer caracter
+print(lista10)
+
+lista10 = "#".join(lista10) # Exemplo adicionando outro caracter
+print(lista10)
 
---
+++ loop_break.py
@@ -0,0 +1,14 @@
+for numero in range(1, 10):
+    if numero == 8:
+        break
+    else: print(f' o número é {numero}')
+print("Saindo")
+
+
+while True:
+    comando = input("digite sair para sair")
+    if comando == 'sair':
+        break
+print("Saindo...")
+
+    
