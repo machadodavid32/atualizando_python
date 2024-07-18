4474a351761341fe566eaf0ccc9eb7c9c586c486
david <machadodavid32@gmail.com>
2024-07-18 10:20:54 -0300

    Terminando treinamento de tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,3 +1,5 @@
+"""
+
 # As tuplas são imutáveis
 
 tupla = (1, 2, 3, 4) # Exemplo de tupla
@@ -22,5 +24,54 @@ print(separar) # Internation
 print(descompactar) # SuperStar
 
 
+tupla4 = (1, 5, 6, 8, 700, 100)
+print(sum(tupla4))
+print(max(tupla4))
+print(min(tupla4))
+print(len(tupla4))
+
+print(5 in tupla4) # número está na tupla4? resposta: True
+
+
+# iterando em uma tupla
+
+for n in tupla4:
+    print(n)
+
+for indice, n in enumerate(tupla4):
+    print(indice, n)
+
+tupla5 = ('a', 'b', 'c', 's', 'g', 'a', 'a')
+print(tupla5.count('a')) # quantos 'a' eu tenho Resposta 3
+print(tupla5.count('b')) # quantos 'b' eu tenho Resposta 1
+
+"""
+
+
+# Transformando uma string em tupla
+
+carro = tuple("Hyundai ix 35")
+print(carro)
+
+# Acessando via indice
+
+print(carro[2]) # resposta u
+
+print(carro[1:5]) 
+
+print(carro[::-1])
+
+# Achando um index
+print(carro.index("x")) # resposta index 9
+
+
+
+
+
+
+
+    
+    
+        
 
 
