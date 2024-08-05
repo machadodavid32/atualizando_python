62867427e15c0031e9bdafb549806f810c49fac6
david <machadodavid32@gmail.com>
2024-08-05 08:02:21 -0300

    atualizando arquivos e modulos collections

--- loop_for.py
+++ loop_for.py
@@ -36,7 +36,7 @@ qtd = int(input("Este aqui, vamos somar, quantas vezes deve rodar?"))
 soma = 0
 
 for n in range(1, qtd+1):
-    num = int(input(f"Informe o número{n}/{qtd} valor"))
+    num = int(input(f"Informe o número{n}/{qtd} valor: "))
     soma = soma + num
     print(f"a soma é {soma}")
     
--- modulos_collections_counter.py
+++ modulos_collections_counter.py
@@ -16,12 +16,11 @@ print(Counter('david')) # Cria elementos e seus valores
 
 texto = '''I wake up in the morning and the first thing I do is brush my teeth.
 After that, I´m going to breakfast and I go up stairs'''
-print(Counter(texto)) # Separando por caracteres
-# Counter({' ': 15, 't': 6, 'e': 5, 'i': 5, 'n': 5, 'h': 5, 'r': 3, 's': 3, 'I': 2, 'a': 2, 'u'
-# : 2, 'm': 2, 'o': 2, 'g': 2, 'd': 2, 'w': 1, 'k': 1, 'p': 1, 'f': 1, 'b': 1, 'y': 1})
 
-palavra = texto.split() # Separando por palavras
-print(Counter(palavra))
-# Counter({'I': 2, 'the': 2, 'wake': 1, 'up': 1, 'in': 1, 'morning': 1, 'and': 1, 'first': 1,
-# 'thing': 1, 'do': 1, 'is': 1, 'brush': 1, 'my': 1, 'teeth': 1})
+# Separando por palavras
+palavras = texto.split()
+contagem_palavras = Counter(palavras)
 
+# Obtendo as 5 palavras mais comuns
+palavras_comuns = contagem_palavras.most_common(5)
+print(palavras_comuns)
